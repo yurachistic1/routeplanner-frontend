b865840db4d8d637ecce5c775ede674c616bde61
Yury Chistov <yurachistic@gmail.com>
2021-10-13 17:51:02 +0100

    add marker and route variable

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 // Map and tiles
-const map = L.map('map').setView([0, 0], 2);
+const map = L.map('map').locate({setView: true, maxZoom: 14})
 
 // add OpenStreetMap tiles
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -7,4 +7,24 @@ const attribution = '&copy; <a href="https://openstreetmap.org/copyright">OpenSt
 const tiles = L.tileLayer(tileUrl, { attribution })
 tiles.addTo(map);
 
-export { map }
+L.control.scale().addTo(map);
+
+// Add marker on click 
+
+let startMarker;
+
+map.on('click', 
+			function(e){
+                if (startMarker){
+                    startMarker.setLatLng(e.latlng)
+                } else {
+                    startMarker = L.marker(e.latlng).addTo(map);
+                }
+				
+			});
+
+// Add route without coordinates 
+
+let route = L.polyline([], {color: 'blue', weight: 4, opacity: 0.55}).addTo(map);
+
+export { map, startMarker, route }
