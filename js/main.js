88caa08f7964d98872440d589436cee67405d73a
Yury Chistov <yurachistic@gmail.com>
2021-10-14 16:55:11 +0100

    add ability to download gpx filex of routes

--- js/main.js
+++ js/main.js
@@ -29,9 +29,10 @@ function generateButton(){
     requestRoutes(lat, lon, distance).then(function success(res){
         routingResult = res
         index = 0
-        document.getElementById('next-run').style.display = 'inline'
+        document.getElementById('top').style.display = 'grid'
         route.setLatLngs(routingResult[0].path)
-        route.bindPopup(`Distance: ${(routingResult[0].distance/1000).toFixed(2)} km.`)
+        document.getElementById("runNo").innerText = `1/${routingResult.length}`
+        document.getElementById("runLen").innerText = `Distance: ${(routingResult[0].distance/1000).toFixed(2)} km.`
     }).catch(function fail(err){
         alert(err)
         return
@@ -40,11 +41,42 @@ function generateButton(){
 }
 
 document.getElementById("next-run").onclick = nextButton;
+document.getElementById("prev-run").onclick = prevButton;
 
 
 function nextButton(){
     index += 1
     index = index % routingResult.length
     route.setLatLngs(routingResult[index].path)
-    route.bindPopup(`Distance: ${(routingResult[index].distance/1000).toFixed(2)} km.`)
+    document.getElementById("runNo").innerText = `${index + 1}/${routingResult.length}`
+    document.getElementById("runLen").innerText = `Distance: ${(routingResult[index].distance/1000).toFixed(2)} km.`
+}
+
+function prevButton(){
+    index -= 1
+    if(index < 0) {
+        index = routingResult.length - 1
+    }
+    route.setLatLngs(routingResult[index].path)
+    document.getElementById("runNo").innerText = `${index + 1}/${routingResult.length}`
+    document.getElementById("runLen").innerText = `Distance: ${(routingResult[index].distance/1000).toFixed(2)} km.`
+}
+
+document.getElementById("download-run").onclick = downloadGPX;
+
+function downloadGPX(){
+    
+    let xml = '<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1" creator="routeplanner"><trk><trkseg>'
+
+    for(const pair of routingResult[index].path){
+        xml += `<trkpt lat="${pair[0]}" lon="${pair[1]}"></trkpt>`
+    }
+
+    xml += '</trkseg></trk></gpx>'
+    const url = 'data:text/json;charset=utf-8,' + xml
+    const link = document.getElementById("download-link")
+    link.download = `${(routingResult[index].distance/1000).toFixed(2)}km.gpx`
+    link.href = url
+
+    link.click()
 }
